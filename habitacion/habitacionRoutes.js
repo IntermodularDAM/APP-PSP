c17413877d97dbb31b4f5c6f9aa41c9427d27bd6
JLD2024 <158503356+JLD2024@users.noreply.github.com>
2025-01-24 10:06:45 +0100

    Update habitacionRoutes.js

--- habitacion/habitacionRoutes.js
+++ habitacion/habitacionRoutes.js
@@ -14,19 +14,19 @@ const {
 
 router.post('/habitaciones', auth, crearHabitacion);
 
-// Obtener todas las reservas (GET)
+// Obtener todas las habitaciones (GET)
 router.get('/habitaciones', obtenerHabitaciones);
 
-// Obtener una reserva por ID (GET)
+// Obtener una habitacion por ID (GET)
 router.get('/habitaciones/:id', obtenerHabitacionPorId);
 
-// Actualizar una reserva completamente (PUT)
+// Actualizar una habitacion completamente (PUT)
 router.put('/habitaciones/:id', actualizarHabitacion);
 
-// Actualizar parcialmente una reserva (PATCH)
+// Actualizar parcialmente una habitacion (PATCH)
 router.patch('/habitaciones/:id', actualizarHabitacionParcial);
 
-// Eliminar una reserva (DELETE)
+// Eliminar una habitacion (DELETE)
 router.delete('/habitaciones/:id', eliminarHabitacion);
 
 module.exports = router;
