d964664be9b9968fa677712d698210952efe262c
JLD2024 <158503356+JLD2024@users.noreply.github.com>
2025-02-03 15:34:24 +0100

    ACTUALIZADO MODEL habitacion.js

--- habitacion/habitacion.js
+++ habitacion/habitacion.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const habitacionSchema = new mongoose.Schema({
     _id: { type: String, unique: true, index: true },
     num_planta: { type: Number, required: true },
+    nombre: {type: String, required: true},
     tipo: { type: String, required: true },
     capacidad: { type: String, required: true },
     descripcion: { type: String, required: true },
@@ -13,8 +14,9 @@ const habitacionSchema = new mongoose.Schema({
     precio_noche: { type: Number, required: true }, // Usamos el mismo nombre que en el backend
     precio_noche_original: { type: Number },         // Precio original (antes de oferta), opcional
     tieneOferta: { type: Boolean, default: false },
-    estado: { type: Boolean, default: true }        // Estado por defecto
-}, { _id: false }); // No generar _id para cada documento individual, se usarÃ¡ el campo _id manualmente.
+    estado: { type: Boolean, default: true }  ,
+    imagenBase64: {type: String}      // Estado por defecto
+}, { _id: false }); // No generar _id para cada documento individual, se usará el campo _id manualmente.
 
 
 habitacionSchema.pre('save', async function (next) {
@@ -27,18 +29,18 @@ habitacionSchema.pre('save', async function (next) {
             .find({ num_planta: habitaciones.num_planta })
             .exec();
 
-        // Generar ID para la nueva habitaciÃ³n
+        // Generar ID para la nueva habitación
         let newId = `H-${(habitaciones.num_planta * 100) + 1}`; // Valor predeterminado
 
         if (habitacionesEnPlanta.length > 0) {
-            // Encontrar el nÃºmero mÃ¡s alto de las habitaciones existentes
+            // Encontrar el número más alto de las habitaciones existentes
             const highestId = Math.max(
                 ...habitacionesEnPlanta.map((habitacion) =>
                     parseInt(habitacion._id.split('-')[1], 10)
                 )
             );
 
-            // Incrementar el nÃºmero secuencial mÃ¡s alto
+            // Incrementar el número secuencial más alto
             newId = `H-${highestId + 1}`;
         }
 
